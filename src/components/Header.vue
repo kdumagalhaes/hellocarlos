01d7bbb008f0e755ac7623ca583aff582f4532bb
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-10-29 18:25:41 -0300

    Add reset css file

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <span class="logo">
+      cp <span class="dot">.</span>
+      <h1>Carlos Pereira - Web developer</h1>
+    </span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  text-transform: uppercase;
+  h1 {
+    font-size: 1px;
+    color: transparent;
+    position: absolute;
+    left: -999px;
+  }
+}
+</style>
