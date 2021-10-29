a93972735ff73d94f3951d6d30f4a839ac32b837
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-10-29 20:52:06 -0300

    Change animation.css import to main.js

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition appear>
+    <main class="hero">
+      <p class="greeting">Hello! My name is</p>
+      <h1 class="name">Carlos Pereira</h1>
+      <p class="bio">
+        I’m a software engineer specializing in building (and occasionally
+        designing) exceptional digital experiences. Currently, I’m focused on
+        building accessible, human-centered products at Upstatement.
+      </p>
+    </main>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
