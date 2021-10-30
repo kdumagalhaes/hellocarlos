69351de90c9ae7152df684f794ee3d352c529995
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-10-29 21:22:41 -0300

    Finish Hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,15 @@
 <template>
-  <transition enter-active-class="animate__animated animate__fadeInUp" appear>
+  <transition enter-active-class="animate__animated animate__slideInUp" appear>
     <main class="hero">
       <p class="greeting">Hello! My name is</p>
       <h1 class="name">Carlos Pereira</h1>
       <p class="bio">
-        I’m a software engineer specializing in building (and occasionally
-        designing) exceptional digital experiences. Currently, I’m focused on
-        building accessible, human-centered products at Upstatement.
+        I’m a software developer student passionate about
+        <strong>frontend</strong> stuff, and sometimes a
+        <strong>musician</strong>. Currently, I’m a
+        <strong>Product Technical Leader</strong> working on physical stores
+        solutions at
+        <a href="https://vtex.com" target="_blank" class="link vtex">vtex</a>.
       </p>
     </main>
   </transition>
@@ -19,4 +22,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.hero {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 400px;
+  padding: 0 40px 0 180px;
+}
+
+.greeting {
+  color: $light-green;
+  font-family: $monospace;
+  font-size: 1.3rem;
+}
+
+.name,
+.bio {
+  color: $light-yellow;
+  margin-bottom: 20px;
+}
+
+.name {
+  font-size: 5rem;
+}
+
+.bio {
+  width: 100%;
+  max-width: 600px;
+  line-height: 26px;
+  color: $light-blue;
+}
+
+.link {
+  color: $light-green;
+  transition: color 0.3s;
+  font-weight: $bold;
+  text-decoration: none;
+
+  &:hover {
+    color: $light-yellow;
+  }
+}
+
+.vtex {
+  text-transform: uppercase;
+}
 </style>
