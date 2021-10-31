41b473fcf71b8964287258713cabd60c1101c297
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-10-31 12:21:32 -0300

    Finish contact section

--- src/components/BasicButton.vue
+++ src/components/BasicButton.vue
@@ -1,7 +1,16 @@
 <template>
-  <button class="button">
+  <a
+    :href="link"
+    target="_blank"
+    class="button"
+    :style="{
+      '--maxWidth': maxWidth,
+      '--height': height,
+      '--fontSize': fontSize,
+    }"
+  >
     {{ text }}
-  </button>
+  </a>
 </template>
 
 <script>
@@ -12,6 +21,22 @@ export default {
       type: String,
       required: true,
     },
+    maxWidth: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      required: true,
+    },
+    fontSize: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
@@ -19,13 +44,19 @@ export default {
 <style lang="scss" scoped>
 .button {
   width: 100%;
-  height: 40px;
+  max-width: var(--maxWidth);
+  height: var(--height);
   background: transparent;
   border: 1px solid $carolina-blue;
   border-radius: 4px;
   padding: 0 20px;
   color: $carolina-blue;
   transition: background 0.3s;
+  text-decoration: none;
+  font-size: var(--fontSize);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   &:hover {
     background: $celadon-blue;
---
+++ src/components/Contact.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="contact" id="contact">
+    <p class="text">
+      If you want to get in touch with me about new opportunities, or just say
+      hi, click on the button below to send me an email!
+    </p>
+    <BasicButton
+      text="Get In Touch"
+      maxWidth="350px"
+      height="70px"
+      fontSize="2rem"
+      link="mailto:ce.magalhaespereira@gmail.com"
+    />
+  </section>
+</template>
+
+<script>
+// component
+import BasicButton from "./BasicButton.vue";
+
+export default {
+  name: "Contact",
+  components: {
+    BasicButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: $safe-area-padding;
+  text-align: center;
+  height: 100vh;
+}
+
+.text {
+  margin-bottom: 40px;
+  color: $sapphire-blue;
+  max-width: 700px;
+  line-height: 26px;
+}
+</style>
