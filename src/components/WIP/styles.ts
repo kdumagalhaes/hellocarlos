a557edd50b99e05e8d03279be9ad58931394548f
Kadu <ce.magalhaespereira@gmail.com>
2022-08-13 10:01:22 -0300

    Add WIP warning

---
+++ src/components/WIP/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gainsboro;
+    width: 100%;
+    height: 100vh;
+
+    .warning-text {
+        font-size: 2rem;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: bold;
+        color: gray;
+    }
+`
