4a13d10baa35cc219af22ab9c5f834931bcb76cb
Kadu <ce.magalhaespereira@gmail.com>
2022-08-13 22:20:55 -0300

    Add Footer section

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { Container } from './styles'
+
+export function Footer() {
+  return (
+    <Container>
+      <footer>
+        <p>
+          Made by me, using <strong>React</strong> + <strong>TypeScript</strong>{' '}
+          :)
+        </p>
+      </footer>
+    </Container>
+  )
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+
+  footer {
+    max-width: 70rem;
+    margin: 0 auto;
+    padding: 1.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.75rem;
+  }
+`
