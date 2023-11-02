62f69ce2b99ca2ae34707b663de8e18ab2788bcd
Carlos Pereira <carlos.pereira@tekever.com>
2023-11-02 19:09:19 +0000

    Update about me section styles

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,6 +1,7 @@
 export const defaultTheme = {
   // colors
   'raisin-black': '#1E1E24',
+  'raisin-darker': '#111117',
   independance: '#484D6D',
   magnolia: '#EFE9F4',
   'forest-green': '#57A773',
