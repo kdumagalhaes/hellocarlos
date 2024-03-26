85284cbd565f2054f1a55b66fa1927d2b80b89bf
Carlos Pereira <carlos.pereira@tekever.com>
2024-03-26 20:53:47 +0000

    Add ProfileName styles

--- src/components/Profile/ProfileName/ProfileName.tsx
+++ src/components/Profile/ProfileName/ProfileName.tsx
@@ -1,8 +1,9 @@
+import styles from './ProfileName.module.scss'
 const ProfileName = () => {
   return (
     <>
-      <h1>Carlos Pereira</h1>
-      <h2>Software Developer</h2>
+      <h1 className={styles.title}>Carlos Pereira</h1>
+      <h2 className={styles['sub-title']}>Software Developer</h2>
     </>
   )
 }
