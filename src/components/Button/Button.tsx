dc062d7028805c1b0051a3c7cd76471eb241b9af
Carlos Pereira <carlos.pereira@tekever.com>
2024-03-26 19:23:17 +0000

    Remove all code :)

--- src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from './styles'
-
-interface ButtonProps {
-  text: string
-}
-
-export function Button({ text }: ButtonProps) {
-  return <Container>{text}</Container>
-}
--- src/components/i18n/locales/index.js
+++
@@ -1,9 +0,0 @@
-import ptBrTranslations from './pt-br'
-import enUsTranslations from './en-us'
-import frFRTranslations from './fr-fr'
-
-export default {
-  'pt-BR': ptBrTranslations,
-  'en-US': enUsTranslations,
-  'fr-FR': frFRTranslations,
-}
