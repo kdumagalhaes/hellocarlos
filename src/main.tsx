f792ce2da4eabad9a9b9a8cc0727ba2a36721988
Carlos Pereira <carlos.pereira@tekever.com>
2024-03-26 19:36:46 +0000

    Add reset styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './global/_global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
